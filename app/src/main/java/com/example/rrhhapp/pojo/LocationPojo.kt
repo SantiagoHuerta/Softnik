9fe58c5dc17c53c41bdfb4456daab02dcc1eeedc
Santiago Huerta <santiagoh.pef.93@gmail.com>
2023-02-13 16:32:00 -0300

    added

---
+++ app/src/main/java/com/example/rrhhapp/pojo/LocationPojo.kt
@@ -0,0 +1,12 @@
+package com.example.rrhhapp.pojo
+
+import java.util.Date
+
+class LocationPojo(
+    var tenant: String,
+    var latitude: Double,
+    var longitude: Double,
+    var date: String,
+    var concept: String // Entry or Exit
+) {
+}
